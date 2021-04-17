35653ee54ef3c73a36076d2e7f64ede5502e9691
Maltinhoo <malthusim5@gmail.com>
2021-04-17 13:56:35 -0300

    enviando app completo

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:buscador_gifs_app/ui/gif_page.dart';
+import 'package:buscador_gifs_app/ui/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: HomePage(),
+    theme: ThemeData(
+        hintColor: Colors.white,
+        primaryColor: Colors.white,
+        inputDecorationTheme: InputDecorationTheme(
+          enabledBorder:
+          OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+          focusedBorder:
+          OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+          hintStyle: TextStyle(color: Colors.white),
+        )),
+  ));
+}
+
